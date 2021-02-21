fb3137b4794c33d27044c3be1c760d5169692058
Maxim Menshikov <maxim@menshikov.org>
2021-02-21 12:36:23 +0300

    [#1] main: exit with the number of errors

--- parslr/__main__.py
+++ parslr/__main__.py
@@ -7,6 +7,6 @@ args = prepare_parser().parse_args()
 p = Parslr(args.antlr, args.tmp_path)
 p.generate_parser(args.grammar)
 p.compile()
-sys.exit(p.run_test_rig(os.path.splitext(os.path.basename(args.grammar))[0],
-                        args.rule,
-                        args.input))
+sys.exit(len(p.run_test_rig(os.path.splitext(os.path.basename(args.grammar))[0],
+                            args.rule,
+                            args.input)))
