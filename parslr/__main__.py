1d2d171cdaa484c6350c004156f2e63144c9a720
Maxim Menshikov <maxim@menshikov.org>
2021-02-21 13:13:38 +0300

    [#2] Parslr: return error code if internal stage fails

--- parslr/__main__.py
+++ parslr/__main__.py
@@ -5,8 +5,14 @@ from parslr.parslr_args import prepare_parser
 
 args = prepare_parser().parse_args()
 p = Parslr(args.antlr, args.tmp_path)
-p.generate_parser(args.grammar)
-p.compile()
+ret_val = p.generate_parser(args.grammar)
+if ret_val != 0:
+	print("Failed to generate parser")
+	sys.exit(ret_val)
+ret_val = p.compile()
+if ret_val != 0:
+	print("Failed to compile parser")
+	sys.exit(ret_val)
 sys.exit(len(p.run_test_rig(os.path.splitext(os.path.basename(args.grammar))[0],
                             args.rule,
                             args.input)))
