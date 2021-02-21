51fd8ab9a494fe31673c39eda8a74b6ac45d2cb9
Maxim Menshikov <maxim@menshikov.org>
2021-02-21 15:49:13 +0300

    [#3] Parslr: support JUnit in main class

--- parslr/Parslr.py
+++ parslr/Parslr.py
@@ -3,6 +3,9 @@ import os
 import subprocess
 import re
 import sys
+import timeit
+import glob
+from junit_xml import TestSuite, TestCase
 
 """
 Main parslr class
@@ -93,3 +96,25 @@ class Parslr:
                 break
 
         return errors
+
+    def run_test_rig_on_dir(self, cls, rule, input_dir, output_xml):
+        if not self.validate():
+            return [Parslr.ERR_PARSER_NOT_READY]
+        files = sorted(glob.glob(os.path.join(input_dir, "*.txt")))
+        failures = 0
+        test_cases = []
+        for file in files:
+            short = os.path.splitext(os.path.basename(file))[0]
+            start = timeit.default_timer()
+            result = self.run_test_rig(cls, rule, file)
+            end = timeit.default_timer()
+            tc = TestCase(short, cls, end - start, '', '', '')
+            if len(result) != 0:
+                tc.add_failure_info("fail", str(result))
+                failures += 1
+            test_cases.append(tc)
+        ts = TestSuite(cls, test_cases)
+        if output_xml != "":
+            with open(output_xml, 'w') as f:
+                TestSuite.to_file(f, [ts])
+        return failures
--- parslr/__main__.py
+++ parslr/__main__.py
@@ -1,8 +1,5 @@
 import sys
 import os
-import timeit
-import glob
-from junit_xml import TestSuite, TestCase
 from parslr.Parslr import Parslr
 from parslr.parslr_args import prepare_parser
 
@@ -19,26 +16,9 @@ if ret_val != 0:
 
 grammar = os.path.splitext(os.path.basename(args.grammar))[0]
 if os.path.isdir(args.input):
-	# A directory with cases
-	test_cases = []
-	files = sorted(glob.glob(os.path.join(args.input, "*.txt")))
-	failures = 0
-	for file in files:
-		short = os.path.splitext(os.path.basename(file))[0]
-		start = timeit.default_timer()
-		result = p.run_test_rig(grammar, args.rule, file)
-		end = timeit.default_timer()
-		tc = TestCase(short, grammar, end - start, '', '', '')
-		if len(result) != 0:
-			tc.add_failure_info("fail", str(result))
-			failures += 1
-		test_cases.append(tc)
-	ts = TestSuite(grammar, test_cases)
-	if args.output != "":
-		with open(args.output, 'w') as f:
-			TestSuite.to_file(f, [ts])
-	if failures > 0:
-		sys.exit(failures)
+    # A directory with cases
+    sys.exit(p.run_test_rig_on_dir(
+        grammar, args.rule, args.input, args.output))
 else:
-	# Just one file
-	sys.exit(len(p.run_test_rig(grammar, args.rule, args.input)))
+    # Just one file
+    sys.exit(len(p.run_test_rig(grammar, args.rule, args.input)))
